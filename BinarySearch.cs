48bce7e2775e69f765aa71e696c7a15372dd4d86
fchang6 <fchang6@gmail.com>
2022-03-15 21:00:24 +0800

    Add BinarySearch

---
+++ BinarySearch.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace ProjectRefresh
+{
+    public class BinarySearch
+    {
+        public int[] MyList { get; set; }
+        public int searchNum { get; set; }
+        public bool result { get; set; }
+        public int resultIndex { get; set; }
+        
+        public BinarySearch(int[] param, int param2)
+        {
+            MyList = param;
+            searchNum = param2;
+            result = false;
+            resultIndex = -1;
+        }
+        public void Run()
+        {
+            binarySearch(0, MyList.Length - 1);
+        }
+        public void binarySearch(int start, int end)
+        {
+            Console.WriteLine($"{start} || {end}");
+            if (start <= end)
+            {
+                int mid = (start + end + (start + end) % 2) / 2;
+                if (MyList[mid] == searchNum)
+                {
+                    result = true;
+                    resultIndex = mid;
+                }
+
+                if (!result)
+                {
+                    if (searchNum < MyList[mid])
+                        binarySearch(start, mid - 1);
+                    else
+                        binarySearch(mid + 1, end);
+
+                }
+
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,21 +7,26 @@ namespace ProjectRefresh
 
         static void Main(string[] args)
         {
-            decimal[] array1 = { 1, 9,15, 4,7,8,13, 3, 2, 9, -4 };
+            //decimal[] array1 = { 1, 9,15, 4,7,8,13, 3, 2, 9, -4 };
+
             //QuickSort qs = new QuickSort(array1);
             //BubbleSort qs = new BubbleSort(array1);
             //InsertionSort qs = new InsertionSort(array1);
-            MergeSort qs = new MergeSort(array1);
-            Console.WriteLine(qs.Print());
+            //MergeSort qs = new(array1);
+            //Console.WriteLine(qs.Print());
             //qs.Swap(2,3);
 
             //Console.WriteLine(qs.Print());
-            qs.Run();
+            //qs.Run();
             //qs.Merge(0, 2, 3,3);
-            Console.WriteLine(qs.Print());
-            Console.WriteLine(qs.PrintNew());
-            Console.WriteLine(qs.Count);
+            //Console.WriteLine(qs.Print());
+            //Console.WriteLine(qs.PrintNew());
+            //Console.WriteLine(qs.Count);
 
+            int[] array1 = new int[] { 1, 2, 5, 8, 9, 13,15,18 };
+            BinarySearch bs = new BinarySearch(array1, 1);
+            bs.Run();
+            Console.WriteLine($"{bs.result} {bs.resultIndex}");
         }
 
     }
